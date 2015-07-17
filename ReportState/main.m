9f1c2f0a6ce0e922527b92b7538f93bdb603672e
陆洋 <luyangandyu@gmail.com>
2015-07-17 16:25:55 +0800

    Initial Commit

---
+++ ReportState/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReportState
+//
+//  Created by 陆洋 on 15/7/17.
+//  Copyright (c) 2015年 luyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
